e17be04af58b79fa8f6f43ebafb3a867ed90a2b0
HeroProtagonist <jstepper91@gmail.com>
2019-09-14 12:07:20 -0400

    Add hide elements to delete function

--- index.js
+++ index.js
@@ -2,11 +2,13 @@
 
 const puppeteer = require('puppeteer');
 const prompts = require('./prompts');
-var page;
+let page;
+
+const { EMAIL, PASSWORD } = process.env
 
 async function main() {
 
-  let answers = await prompts();
+  const answers = await prompts();
 
   const browser = await puppeteer.launch({
     headless: false,
@@ -15,8 +17,8 @@ async function main() {
   page = await browser.newPage();
 
   await page.goto('https://mbasic.facebook.com/');
-  await page.$eval('input[id=m_login_email]', (el, user) => el.value = user, answers.username);
-  await page.$eval('input[name=pass]', ((el, pass) => el.value = pass), answers.password);
+  await page.$eval('input[id=m_login_email]', (el, user) => el.value = user, EMAIL || answers.username);
+  await page.$eval('input[name=pass]', ((el, pass) => el.value = pass), PASSWORD || answers.password);
   await page.$eval('input[name=login]', button => button.click());
   await page.goto('https://mbasic.facebook.com/');
 
@@ -28,10 +30,10 @@ async function next(categories, years) {
   await followLinkByContent('Activity Log');
   await followLinkByContent('Filter');
 
-  for (let i in categories) {
+  for (const i in categories) {
     console.log("Deleting category " + categories[i]);
     await followLinkByContent(categories[i]);
-    for (let j in years) {
+    for (const j in years) {
       console.log("In year " + years[j]);
       try {
         await followLinkByContent(years[j]);
@@ -50,15 +52,22 @@ async function next(categories, years) {
 
 async function deletePosts() {
   // get all "allactivity/delete" and "allactivity/removecontent" links on page
-  var deleteLinks = await page.evaluate(() => {
-    var links = [];
+  const deleteLinks = await page.evaluate(() => {
+    const links = [];
     const deleteElements = document.querySelectorAll('a[href*="allactivity/delete"]');
     const removeElements = document.querySelectorAll('a[href*="allactivity/removecontent"]');
+    const hideElements = document.querySelectorAll('a[href*="allactivity/visibility"]');
+
     for (const el of deleteElements) {
         links.push(el.href);
     }
     for (const el of removeElements) {
         links.push(el.href);
+    }
+    for (const el of hideElements) {
+        if (el.innerText.includes('Hide')) {
+          links.push(el.href);
+        }
     }
     return links;
   });
@@ -70,12 +79,12 @@ async function deletePosts() {
 
 
 async function getMonthLinks(year) {
-  var monthLinks = await page.evaluate((year) => {
-    var months = ["January", "February", "March", "April", "May", "June", 
+  const monthLinks = await page.evaluate((year) => {
+    const months = ["January", "February", "March", "April", "May", "June",
       "July", "August", "September", "October", "November", "December"];
-    var links = [];
+    const links = [];
     const elements = document.querySelectorAll('a');
-    for (let el of elements) {
+    for (const el of elements) {
       for (let i = 0; i < months.length; i++) {
         if (months[i] + " " + year === el.innerText) {
           links.push(el.href);
@@ -88,7 +97,8 @@ async function getMonthLinks(year) {
 }
 
 async function followLinkByContent(content) {
-  var link = await page.evaluate((text) => {
+  const link = await page.evaluate((text) => {
+    console.log(text)
     const aTags = document.querySelectorAll('a');
     for (let aTag of aTags) {
       if (aTag.innerText === text) {
@@ -100,8 +110,8 @@ async function followLinkByContent(content) {
 }
 
 async function deleteYear(year) {
-  var monLinks = await getMonthLinks(year);
-  for (let mon in monLinks) {
+  const monLinks = await getMonthLinks(year);
+  for (const mon in monLinks) {
     // console.log("Deleting month: ", monLinks[mon]);
     await page.goto(monLinks[mon]);
     await deletePosts();
